2dbe314c5e6623a4b11d5a4a1e04d2592c5b50f1
Pranjli Singhal <pranjlisinghal@gmail.com>
2020-10-03 22:24:51 +0530

    Updates

---
+++ precache-manifest.6e38eeb712ac3c1dafcf26534a16220f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2845705d4a4d054f71c55c16a6f7305",
+    "url": "/PranjliSinghal.github.io/index.html"
+  },
+  {
+    "revision": "f64b459cd92b1afccfd2",
+    "url": "/PranjliSinghal.github.io/static/js/2.415ac386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PranjliSinghal.github.io/static/js/2.415ac386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a1fdccb9ab4ace8909",
+    "url": "/PranjliSinghal.github.io/static/js/main.9736b95a.chunk.js"
+  },
+  {
+    "revision": "a12d958b56868da83a54",
+    "url": "/PranjliSinghal.github.io/static/js/runtime-main.fead400f.js"
+  },
+  {
+    "revision": "8d546da6783cf3010b5a1c791911106e",
+    "url": "/PranjliSinghal.github.io/static/media/Color-tracker.8d546da6.jpg"
+  },
+  {
+    "revision": "23d26161be27bf32c93f36475bb38530",
+    "url": "/PranjliSinghal.github.io/static/media/Face-Classifier.23d26161.jpg"
+  },
+  {
+    "revision": "902781cda55ab3f6b9e5ba773b2d726b",
+    "url": "/PranjliSinghal.github.io/static/media/Fake-news.902781cd.jpg"
+  },
+  {
+    "revision": "6bab44fcf970b2e4ee2bc9c6fc7be903",
+    "url": "/PranjliSinghal.github.io/static/media/ML.6bab44fc.jpg"
+  },
+  {
+    "revision": "330ff7b08eb44e65056bc8a94c66482d",
+    "url": "/PranjliSinghal.github.io/static/media/Me.330ff7b0.jpg"
+  },
+  {
+    "revision": "93de43f8feb482ae1cf5255d35155234",
+    "url": "/PranjliSinghal.github.io/static/media/Motion.93de43f8.jpg"
+  },
+  {
+    "revision": "b4d7fdc028a67d34609faf57faf2a0ab",
+    "url": "/PranjliSinghal.github.io/static/media/MovieReview.b4d7fdc0.jpg"
+  }
+]);
